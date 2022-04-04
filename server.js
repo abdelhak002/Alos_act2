45f5d20f2631f6e0e767a0dec27bb2c66aa934f8
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-04-04 17:47:52 +0100

    minor changes

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3002;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
-
+app.use(express.static("./routes/web"));
 app.use("/api/recipes", require("./routes/api/recipes"));
+
 app.listen(port, () => {
   console.log(`Site running on port ${port}`);
 });
